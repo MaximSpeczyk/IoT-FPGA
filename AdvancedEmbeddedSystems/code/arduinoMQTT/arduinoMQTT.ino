173343eafa27adbe3854f6b6d84fc5bd773b118f
MaximSpeczyk <91470066+MaximSpeczyk@users.noreply.github.com>
2023-05-15 12:34:01 +0200

    Update arduinoMQTT.ino

--- AdvancedEmbeddedSystems/code/arduinoMQTT/arduinoMQTT.ino
+++ AdvancedEmbeddedSystems/code/arduinoMQTT/arduinoMQTT.ino
@@ -14,5 +14,67 @@ const char topic[] = "MAXIM";
 
 void setup()
 {
-  Serial.begin
+  Serial.begin(9600);
+  while(!Serial)
+  {
+    ;
+  }
+
+  Serial.print("Connecting to SSID; ")
+  Serial.println(ssid);
+  
+  while(WiFi.begin(ssid,pass) != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(4000(;)
+  }
+
+  Serial.println("Congrats, you are connected :)");
+  Serial.println();
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) 
+  {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while(1);
+  }
+
+  Serial.println("You are connected to the MQTT broker :)");
+  Serial.println();
+
+  mqttClient.onMessage(onMqttMessage);
+
+  Serial.print("Subscribing to topic: ");
+  Serial.println(topic);
+  Serial.println();
+
+  mqttClient.subscribe(topic);
+
+  Serial.println();
+}
+
+void loop()
+{
+  mqttClient.poll();
+}
+
+void onMqttMessage(int messageSize) 
+{
+  Serial.println("Received a message with topic '");
+  Serial.print(mqttClient.messageTopic());
+  Serial.print("', length ");
+  Serial.print(messageSize);
+  Serial.println(" bytes:");
+
+  while (mqttClient.available()) 
+  {
+    Serial.print((char)mqttClient.read());
+  }
+  
+  Serial.println();
+  Serial.println();
 }
