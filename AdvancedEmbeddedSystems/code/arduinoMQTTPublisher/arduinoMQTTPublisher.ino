ff206d06395a6f7707e6ae6414daf68f80dd4a59
MaximSpeczyk <91470066+MaximSpeczyk@users.noreply.github.com>
2023-05-23 19:54:16 +0200

    Update arduinoMQTTPublisher.ino

--- AdvancedEmbeddedSystems/code/arduinoMQTTPublisher/arduinoMQTTPublisher.ino
+++ AdvancedEmbeddedSystems/code/arduinoMQTTPublisher/arduinoMQTTPublisher.ino
@@ -1,9 +1,78 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ArduinoMqttClient.h>
+#include <WiFiNINA.h>
+#include "passwordMQTT.h"
+#include <dht11.h> 
 
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+
+WiFiClient wifiClient;
+MqttClient mqttClient(wifiClient);
+
+const char broker[] = "192.168.0.105";
+int        port     = 1883;
+const char topic[]  = "MAXIM";
+
+const long interval = 8000;
+unsigned long previousMillis = 0;
+
+int count = 0;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }
+
+
+  Serial.print("Attempting to connect to WPA SSID: ");
+  Serial.println(ssid);
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+
+    Serial.print(".");
+    delay(5000);
+  }
+
+  Serial.println("You're connected to the network");
+  Serial.println();
+
+  Serial.print("Attempting to connect to the MQTT broker: ");
+  Serial.println(broker);
+
+  if (!mqttClient.connect(broker, port)) {
+    Serial.print("MQTT connection failed! Error code = ");
+    Serial.println(mqttClient.connectError());
+
+    while (1);
+  }
+
+  Serial.println("You're connected to the MQTT broker!");
+  Serial.println();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  mqttClient.poll();
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+
+    previousMillis = currentMillis;
+
+
+    int Rvalue = analogRead(A0);
+    int Rvalue2 = analogRead(A1);
+    int Rvalue3 = analogRead(A2);
+
+    Serial.print("Sending message to topic: ");
+    Serial.println(topic);
+    Serial.println(Rvalue);
+
+    mqttClient.beginMessage(topic);
+    mqttClient.print(Rvalue);
+    mqttClient.endMessage();
+
+    Serial.println();
+  }
 }
