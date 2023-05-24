8882559300b7e8b72adee008a24429329d59738f
MaximSpeczyk <maxim-emile.speczyk@stud.hshl.de>
2023-05-24 14:52:14 +0200

    Update arduinoMQTTPublisher.ino

--- AdvancedEmbeddedSystems/code/arduinoMQTTPublisher/arduinoMQTTPublisher.ino
+++ AdvancedEmbeddedSystems/code/arduinoMQTTPublisher/arduinoMQTTPublisher.ino
@@ -13,7 +13,7 @@ char pass[] = SECRET_PASS;
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 
-const char broker[] = "192.168.0.105";
+const char broker[] = "raspberrypi.local";
 int port = 1883;
 const char dhtTopic[] = "DHT11";
 const char lightTopic[] = "light";
